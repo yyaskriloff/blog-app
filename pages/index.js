a66c6cf583e8a689419b22dab02b48143b2e5f2d
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2023-01-15 23:23:07 +0200

    polishing index page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -30,11 +34,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
